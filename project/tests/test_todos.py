1d2477d09081d13b4c52b098fb4f1c13f5f0fd7c
Clodo <claudiobidau@gmail.com>
2017-12-22 13:10:41 -0300

    First commit

---
+++ project/tests/test_todos.py
@@ -0,0 +1,127 @@
+from unittest.mock import Mock, patch
+
+from nose.tools import assert_is_none, assert_is_not_none, assert_list_equal, assert_true
+
+from project.services import get_todos
+from project.services import get_uncompleted_todos
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_alternative_one(mock_get):
+    # Configure the mock to return a response with OK status code.
+    mock_get.return_value.ok = True
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
+
+
+def test_getting_todos_alternative_two():
+    with patch('project.services.requests.get') as mock_get:
+        # Configure the mock to return a response with an OK status code.
+        mock_get.return_value.ok = True
+
+        # Call the service, which will send a request to the server.
+        response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
+
+
+def test_getting_todos_alternative_three():
+    mock_get_patcher = patch('project.services.requests.get')
+
+    # Start patching `requests.get`.
+    mock_get = mock_get_patcher.start()
+
+    # Configure the mock to return a response with an OK status code.
+    mock_get.return_value.ok = True
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # Stop patching `requests.get`.
+    mock_get_patcher.stop()
+
+    # If the request is sent successfully, then I expect a response to be returned.
+    assert_is_not_none(response)
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_ok(mock_get):
+    todos = [{
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }]
+
+    # Configure the mock to return a response with an OK status code. Also, the
+    # mock should have a `json()` method that returns a list of todos.
+    mock_get.return_value = Mock(ok=True)
+    mock_get.return_value.json.return_value = todos
+
+    # Call the service, which will send a request to the server.
+    response = get_todos()
+
+    # If the request is sent successfully, then I expect a response to be
+    # returned.
+    assert_list_equal(response.json(), todos)
+
+
+@patch('project.services.requests.get')
+def test_getting_todos_when_response_is_not_ok(mock_get):
+    # Configure the mock to not return reponse with an OK status code.
+    mock_get.return_value.ok = False
+
+    # Call the service, shich will send a request to the server.
+    response = get_todos()
+
+    # If the response contains an error, I should get no todos.
+    assert_is_none(response)
+
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_not_none(mock_get_todos):
+    todo1 = {
+        'userId': 1,
+        'id': 1,
+        'title': 'Make the bed',
+        'completed': False
+    }
+    todo2 = {
+        'userId': 1,
+        'id': 2,
+        'title': 'Walk the dog',
+        'completed': True
+    }
+
+    # Configure mock to return a response with a JSON-serialized list of todos.
+    mock_get_todos.return_value = Mock()
+    mock_get_todos.return_value.json.return_value = [todo1, todo2]
+
+    # Call the service, which will get a list of todos filtered on completed.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert_true(mock_get_todos.called)
+
+    # Confirm that the expected filtered list of todos was returned.
+    assert_list_equal(uncompleted_todos, [todo1])
+
+
+@patch('project.services.get_todos')
+def test_getting_uncompleted_todos_when_todos_is_none(mock_get_todos):
+    # Configure mock to return None.
+    mock_get_todos.return_value = None
+
+    # Call the service, which will return an empty list.
+    uncompleted_todos = get_uncompleted_todos()
+
+    # Confirm that the mock was called.
+    assert_true(mock_get_todos.called)
+
+    # Confirm that an empty list was returned.
+    assert_list_equal(uncompleted_todos, [])
